0cc6d5d23d14aab6f9aa9dbfc2750d84d81f100f
SHAILAJA TIWARI <imshelly61003@gmail.com>
2026-01-02 11:53:48 +0530

    Resolved merge conflict in uploadController

--- Asset-Management-system/backend/Config/mysql.js
+++ Asset-Management-system/backend/Config/mysql.js
@@ -5,7 +5,7 @@ import mysql from "mysql2/promise";
 export const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "9835397556",        // put your MySQL password if you have one
+  password: "shally2003@@",        // put your MySQL password if you have one
   database: "asset_management",
   waitForConnections: true,
   connectionLimit: 10,
